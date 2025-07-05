e7ff0d17facf5ce5c26bfe6e321d075db8a75360
Tyler Baylson <thbaylson@gmail.com>
2025-07-04 23:47:58 -0400

    Lesson 1.7: Camera Zoom. Pressing the end key will zoom in. Releasing the end key will automatically zoom out.

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,12 +1,79 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Transform cameraTarget;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
     [SerializeField] private float keyboardPanSpeed = 5f;
+    [SerializeField] private float zoomSpeed = 1f;
+    [SerializeField] private float minZoomDistance = 7.5f;
+
+    private CinemachineFollow cinemachineFollow;
+    // This is used to determine the furthest the camera can zoom out.
+    private Vector3 startingFollowOffset;
+    private float zoomStartTime;
+
+    private void Awake()
+    {
+        if (!cinemachineCamera.TryGetComponent(out cinemachineFollow))
+        {
+            Debug.LogError("CinemachineCamera does not have a CinemachineFollow component.");
+            return;
+        }
+
+        startingFollowOffset = cinemachineFollow.FollowOffset;
+    }
 
     void Update()
+    {
+        HandlePanning();
+        HandleZooming();
+    }
+
+    private void HandleZooming()
+    {
+        if (ShouldSetZoomStartTime())
+        {
+            zoomStartTime = Time.time;
+        }
+
+        // We clamp here to be safe since we are using Time.time
+        float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+        Vector3 targetFollowOffset;
+
+        // If the end key is pressed, we zoom in from where we are towards the minimum distance.
+        if (Keyboard.current.endKey.isPressed)
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                minZoomDistance,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+        // If the end key is released, we zoom out from where we are towards the starting offset.
+        else
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                startingFollowOffset.y,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+
+        // Slerp = Spherical Linear Interpolation. Gives a more natural zooming effect.
+        cinemachineFollow.FollowOffset = Vector3.Slerp(
+            cinemachineFollow.FollowOffset,
+            targetFollowOffset,
+            zoomTime
+        );
+    }
+
+    private static bool ShouldSetZoomStartTime() => Keyboard.current.endKey.wasPressedThisFrame
+        || Keyboard.current.endKey.wasReleasedThisFrame;
+
+    private void HandlePanning()
     {
         Vector2 moveAmount = Vector2.zero;
         // TODO: There's gotta be a better way. Is there a device-independent way to get input?
