81eefb5bd33ba1854c0046d8a682baa51f9806b0
Tyler Baylson <thbaylson@gmail.com>
2026-01-20 17:14:47 -0500

    Lesson 4.4: Gatherable Supplies. Created ScriptableObjects for supplies that can be gathered from the environment.

---
+++ Assets/Scripts/Environment/GatherableSupply.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace RTS.Environment
+{
+    public class GatherableSupply : MonoBehaviour, IGatherable
+    {
+        [field: SerializeField] public SupplySO Supply { get; private set; }
+        [field: SerializeField] public int Amount { get; private set; }
+        [field: SerializeField] public bool IsBusy { get; private set; }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            Amount = Supply.MaxAmount;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public bool BeginGather()
+        {
+            if (IsBusy) return false;
+
+            IsBusy = true;
+            return true;
+        }
+
+        public int EndGather()
+        {
+            IsBusy = false;
+            int amountGathered = Mathf.Min(Supply.AmountPerGather, Amount);
+            Amount -= amountGathered;
+
+            if(Amount <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            return amountGathered;
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/IGatherable.cs
@@ -0,0 +1,12 @@
+namespace RTS.Environment
+{
+    public interface IGatherable
+    {
+        public SupplySO Supply { get; }
+        public int Amount { get; }
+        public bool IsBusy { get; }
+
+        public bool BeginGather();
+        public int EndGather();
+    }
+}
